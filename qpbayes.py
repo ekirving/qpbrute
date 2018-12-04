a554fa05dcb6004679c060264a47c505eb0c523e
Evan <ekirving@hotmail.com>
2018-12-04 14:28:27 +0000

    updates to calculate_dstats()

--- qpbayes.py
+++ qpbayes.py
@@ -25,24 +25,39 @@ from consts import *
 
 class QPBayes:
 
-    def __init__(self, graph_names, log_file, dot_path, nodes, outgroup, verbose, nthreads):
+    def __init__(self, geno, snp, ind, prefix, nodes, outgroup, verbose, nthreads):
         """
         Initialise the object attributes
         """
-        self.graph_names = graph_names
-        self.log_file = log_file
-        self.dot_path = dot_path
+        self.geno_file = geno
+        self.snp_file = snp
+        self.ind_file = ind
+        self.prefix = prefix
+        self.nodes = nodes
+        self.outgroup = outgroup
         self.verbose = verbose
         self.nthreads = nthreads
 
-        # open the file for writing
-        self.log_handle = open(log_file, 'a')
-
+        # sanity check the outgroup is not in the node list
         if outgroup in nodes:
-            nodes.remove(outgroup)
+            self.nodes.remove(outgroup)
 
-        self.nodes = nodes
-        self.outgroup = outgroup
+        # find all the PDFs, and extract their graph names
+        self.graphs = [re.search(r'a[0-9]-(.+).pdf', pdf).group(1) for pdf in glob.glob('pdf/{}*'.format(prefix))]
+
+        # self.dot_path = 'graphs/{}'.format(prefix)
+        self.par_file = "dstats/{}.par".format(prefix)
+        self.csv_file = "dstats/{}.csv".format(prefix)
+        self.dstat_file = "dstats/{}.log".format(prefix)
+        self.tests_file = "dstats/{}.tests".format(prefix)
+        self.log_file = '{}.bayes.log'.format(prefix)
+
+        # clean up the log file
+        if os.path.exists(self.log_file):
+            os.remove(self.log_file)
+
+        # open the log file for writing
+        self.log_handle = open(self.log_file, 'a')
 
     def log(self, message):
         """
@@ -59,23 +74,14 @@ class QPBayes:
 
     def calculate_dstats(self):
         """
-        Use AdmixTools to calculate D-stats for all possible permutations of populations.
+        Use `qpDstat` to calculate D-stats for all possible permutations of populations.
 
         See https://github.com/DReichLab/AdmixTools/blob/master/README.Dstatistics
         """
 
-        geno_file = "pygmyhog/autosomes.filtered.geno"
-        snp_file = "pygmyhog/autosomes.filtered.snp"
-        ind_file = "pygmyhog/autosomes.filtered.ind"
-        par_file = "pygmyhog/autosomes.filtered.dstat.par"
-
-        tests_file = 'pygmyhog/autosomes.filtered.tests'
-        dstat_file = 'pygmyhog.dstat'
-        csv_file = 'pygmyhog.csv'
-
         # get all the samples, grouped by population
         samples = defaultdict(list)
-        with open(ind_file, 'r') as fin:
+        with open(self.ind_file, 'r') as fin:
             for line in fin.readlines():
                 sample, gender, population = line.split()
                 samples[population].append(sample)
@@ -87,29 +93,29 @@ class QPBayes:
             tests.add((self.outgroup, x, y, z))
 
         # write all the tests to disk
-        with open(tests_file, 'w') as fout:
+        with open(self.tests_file, 'w') as fout:
             fout.writelines(' '.join(test) + '\n' for test in tests)
 
         # compose the config settings
         config = [
-            "genotypename: {}".format(geno_file),
-            "snpname:      {}".format(snp_file),
-            "indivname:    {}".format(ind_file),
-            "popfilename:  {}".format(tests_file),  # Program will run the method for all listed 4-way tests
+            "genotypename: {}".format(self.geno_file),
+            "snpname:      {}".format(self.snp_file),
+            "indivname:    {}".format(self.ind_file),
+            "popfilename:  {}".format(self.tests_file),  # Program will run the method for all listed 4-way tests
             "blgsize:      0.005"                   # TODO parameterise
         ]
 
         # the params to be defined in a .par file
-        with open(par_file, 'w') as fout:
+        with open(self.par_file, 'w') as fout:
             fout.write("\n".join(config))
 
-        self.log("INFO: There are {:,} D-stat tests to compute".format(len(tests)))
+        self.log("INFO: There are {:,} D-stat tests to compute for {} populations.".format(len(tests), len(self.nodes)))
 
         # run qpDstat
-        log = run_cmd(["qpDstat", "-p", par_file])
+        log = run_cmd(["qpDstat", "-p", self.par_file])
 
         # save the log file
-        with open(dstat_file, 'w') as fout:
+        with open(self.dstat_file, 'w') as fout:
             fout.write(log)
 
         results = list()
@@ -121,44 +127,48 @@ class QPBayes:
                 results.append(dict(zip(columns, line.split()[1:7])))
 
         # convert to DataFrame and save to disk
-        pd.DataFrame(results, columns=columns).to_csv(csv_file, index=False)
+        pd.DataFrame(results, columns=columns).to_csv(self.csv_file, index=False)
+
+    def calculate_bayes_factors(self):
+        """
+        Use `admixturegraph` to calculate Bayes factors for all fitted graphs.
+
+        See https://github.com/mailund/admixture_graph
+        """
+
+        self.log("INFO: There are {:,} graphs to compute Bayes factors for.".format(len(self.graphs)))
+
+        pass
 
 
-def calculate_bayes_factors(graph_names, prefix, nodes, outgroup, verbose=True, nthreads=CPU_CORES_MAX):
+def calculate_bayes_factors(geno, snp, ind, prefix, nodes, outgroup, verbose=True, nthreads=CPU_CORES_MAX):
     """
     Find the best fitting graph by calculating the Bayes factors for each model found by QPBrute.
     """
     start = time()
 
-    log_file = '{}.bayes.log'.format(prefix)
-    dot_path = 'graphs/{}'.format(prefix)
-
-    # clean up the log file
-    if os.path.exists(log_file):
-        os.remove(log_file)
-
     # instantiate the class
-    qpb = QPBayes(graph_names, log_file, dot_path, nodes, outgroup, verbose, nthreads)
+    qpb = QPBayes(geno, snp, ind, prefix, nodes, outgroup, verbose, nthreads)
 
-    qpb.log("INFO: There are {:,} graphs to compute Bayes factors for".format(len(set(graph_names))))
-
-    # get all the D-stat permutations
+    # calculate outgroup D-stats for all 3-way permutations of the populations
     qpb.calculate_dstats()
 
-    qpb.log("INFO: Calculating Bayes factors took: %s" % timedelta(seconds=time() - start))
+    # calculate Bayes factors for all the fitted graphs, using the pre-computed D-stats
+    qpb.calculate_bayes_factors()
+
+    qpb.log("INFO: Calculating Bayes factors took: {}".format(timedelta(seconds=time() - start)))
 
 
 if __name__ == "__main__":
     # parse the command line arguments
     parser = argparse.ArgumentParser(description='Compare all fitted models to each other using Bayes factors.')
+    parser.add_argument("--geno", help="Input genotype file (in eigenstrat format)", metavar='example.geno', required=True)
+    parser.add_argument("--snp", help="Input snp file (in eigenstrat format)", metavar='example.snp', required=True)
+    parser.add_argument("--ind", help="Input indiv file (in eigenstrat format)", metavar='example.ind', required=True)
     parser.add_argument("--prefix", help="Output prefix", metavar='example', required=True)
     parser.add_argument("--pops", nargs='+', help='List of populations', metavar=('A', 'B'), required=True)
     parser.add_argument("--out", help="Outgroup population", metavar='OUT', required=True)
 
     argv = parser.parse_args()
 
-    # find all the PDFs, and extract the graph names
-    files = glob.glob('pdf/{}*'.format(argv.prefix))
-    graphs = [re.search(r'a[0-9]-(.+).pdf', filename).group(1) for filename in files]
-
-    calculate_bayes_factors(graphs, argv.prefix, argv.pops, argv.out)
+    calculate_bayes_factors(argv.geno, argv.snp, argv.ind, argv.prefix, argv.pops, argv.out)
