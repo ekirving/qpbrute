bac6b81175fc1b46782058d2901dd832d61921f2
Evan <ekirving@hotmail.com>
2018-12-14 14:14:37 +0000

    tweak to multi

--- consts.py
+++ consts.py
@@ -9,7 +9,7 @@ TOTAL_CORES = multiprocessing.cpu_count()
 # no single worker should use more than 30% of the available cores
 CPU_CORES_MED = int(TOTAL_CORES * 0.3)   # 30%
 CPU_CORES_HIGH = int(TOTAL_CORES * 0.5)  # 50%
-CPU_CORES_MAX = int(TOTAL_CORES * 0.9)   # 90%
+CPU_CORES_MAX = TOTAL_CORES
 
 # default colours for print the graphs
 COLOURS = ['#1f78b4', '#33a02c', '#e31a1c', '#ff7f00', '#6a3d9a', '#b15928', '#a6cee3', '#b2df8a', '#fb9a99', '#fdbf6f',
